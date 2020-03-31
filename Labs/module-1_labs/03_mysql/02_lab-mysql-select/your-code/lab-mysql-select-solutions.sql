139103793cf1f136fcea72a6254f6c66fff84c97
tessajean <31619981+tessajean@users.noreply.github.com>
2020-03-31 18:35:05 +0200

    fix Bonus Challenge solution

--- Labs/module-1_labs/03_mysql/02_lab-mysql-select/your-code/lab-mysql-select-solutions.sql
+++ Labs/module-1_labs/03_mysql/02_lab-mysql-select/your-code/lab-mysql-select-solutions.sql
@@ -40,7 +40,7 @@ order by TOTAL desc;
 
 select au_id as "AUTHOR ID", au_lname as "LAST NAME", au_fname as "FIRST NAME", sum(advance + ROYALTIES) as PROFITS from (
 	select title_id, au_id, au_lname, au_fname, advance, sum(ROYALTIES) as ROYALTIES from (
-		select t.title_id, t.price, t.advance, t.royalty, s.qty, a.au_id, au_lname, au_fname, ta.royaltyper, (t.price * s.qty * t.royalty * ta.royaltyper / 10000) as ROYALTIES
+		select t.title_id, t.price, t.advance * (ta.royaltyper / 100) as advance, t.royalty, s.qty, a.au_id, au_lname, au_fname, ta.royaltyper, (t.price * s.qty * t.royalty * ta.royaltyper / 10000) as ROYALTIES
 		from titles t
 		inner join sales s on s.title_id = t.title_id
 		inner join titleauthor ta on ta.title_id = s.title_id
@@ -54,16 +54,26 @@ limit 3;
 
 -- Alternative Solution
 
-SELECT authors.au_id AS AUTHOR_ID, au_lname AS LAST_NAME, au_fname AS FIRST_NAME, titles.title as TITLE, titles.title_id as TITLE_ID, titles.price as PRICE,
-	sum(sales.qty) as QUANTITY,
-	sum(sales.qty)*titles.price as REVENUE,
-	titles.advance as ADVANCE,
-	titleauthor.royaltyper*titleauthor.royaltyper as ROYALTYS,
-	COALESCE((SUM(sales.qty)*titles.price*titles.royalty+titles.advance)*titleauthor.royaltyper*0.01, 0) as TOTAL  -- calculate how much each book will earn, and multiply by royalty per author (royaltyper)
-	FROM authors
-		LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
-		LEFT JOIN titles ON titleauthor.title_id = titles.title_id
-		LEFT JOIN sales ON sales.title_id = titles.title_id
-GROUP BY authors.au_id, titles.title_id, titles.price, titleauthor.royaltyper
-order by TOTAL desc
-limit 3 ;
+WITH profit_per_title AS (
+    SELECT
+        s.title_id,
+        t.advance,
+        t.price * (t.royalty / 100) AS royalty_per_sale,
+        SUM(s.qty)                  AS titles_sold
+    FROM sales s
+        JOIN titles t
+        ON s.title_id = t.title_id
+    GROUP BY 1,2,3)
+SELECT
+    t.au_id,
+    a.au_lname,
+    a.au_fname,
+    SUM((ept.advance * (t.royaltyper / 100)) +
+    (ept.royalty_per_sale * ept.titles_sold * (t.royaltyper / 100)))  AS profit
+FROM titleauthor t
+    JOIN profit_per_title ept
+    ON t.title_id = ept.title_id
+    JOIN authors a
+    ON t.au_id = a.au_id
+GROUP BY 1,2,3
+ORDER BY profit DESC;
